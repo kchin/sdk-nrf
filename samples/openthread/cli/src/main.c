6eda37ad199f2c50d3e532c26cb9a92cb1e6c023
Kian-Vine Chin <KianVineChin@yahoo.com>
2021-03-25 21:14:15 +0000

    [nrf52840dongle] Use USB CDC_ACM for OT interaction

--- samples/openthread/cli/src/main.c
+++ samples/openthread/cli/src/main.c
@@ -7,6 +7,12 @@
 #include <zephyr.h>
 #include <logging/log.h>
 
+#include <sys/printk.h>
+#include <sys/util.h>
+#include <string.h>
+#include <usb/usb_device.h>
+#include <drivers/uart.h>
+
 #if CONFIG_BT
 #include "ble.h"
 #endif
@@ -25,10 +31,31 @@ LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
 
 void main(void)
 {
+	const struct device *dev = device_get_binding(
+			CONFIG_UART_CONSOLE_ON_DEV_NAME);
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return;
+	}
+
+	/* Poll if the DTR flag was set, optional */
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+	}
+
+	if (strlen(CONFIG_UART_CONSOLE_ON_DEV_NAME) !=
+	    strlen("CDC_ACM_0") ||
+	    strncmp(CONFIG_UART_CONSOLE_ON_DEV_NAME, "CDC_ACM_0",
+		    strlen(CONFIG_UART_CONSOLE_ON_DEV_NAME))) {
+		printk("Error: Console device name is not USB ACM\n");
+
+		return;
+	}
+
 	LOG_INF(WELLCOME_TEXT);
 
 #if CONFIG_BT
 	ble_enable();
 #endif
-
 }
