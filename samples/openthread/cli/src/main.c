601e144639ac23083b7f525346ab75c700a81586
Kian-Vine Chin <KianVineChin@yahoo.com>
2021-03-27 00:18:34 +0000

    [nrf52840dongle] Backout part of the previous simplication.  Turns out they are needed for the uart to work.

--- samples/openthread/cli/src/main.c
+++ samples/openthread/cli/src/main.c
@@ -9,6 +9,7 @@
 
 #if defined(CONFIG_SOC_NRF52840)
 #include <usb/usb_device.h>
+#include <drivers/uart.h>
 #endif
 
 #if CONFIG_BT
@@ -30,9 +31,18 @@ LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
 void main(void)
 {
 #if defined(CONFIG_SOC_NRF52840)
+	const struct device *dev = device_get_binding(
+			CONFIG_UART_CONSOLE_ON_DEV_NAME);
+	uint32_t dtr = 0;
+
 	while (usb_enable(NULL)) {
       // retry if failed
       k_sleep(K_SECONDS(1));
+	}
+
+	/* Poll if the DTR flag was set, optional */
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
 	}
 #endif
 
